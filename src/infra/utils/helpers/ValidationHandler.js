d6248c805b1ac69468ae166c001ea8a726ccb0e4
Levy Silva <levysilva.dev@gmail.com>
2024-10-13 16:42:38 -0300

    feat: add ValidationHandler class for validation error handling

---
+++ src/infra/utils/helpers/ValidationHandler.js
@@ -0,0 +1,9 @@
+const Responser = require("./Responser.js");
+
+class ValidationHandler extends Responser {
+    constructor(message) {
+        super(message || "A validation error occurred.", 400);
+    }
+}
+
+module.exports = ValidationHandler;
