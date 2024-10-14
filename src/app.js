31edb5db4b703467cff2adf56624a3dee7c47a02
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 10:44:47 -0300

    feat: import router, database and errorHandler to app entry point

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const router = require("./adapters/routes/router.js");
+const errorHandler = require("./infra/middlewares/errorHandler.js");
+
+require("./infra/database/models/index.js");
 
 const app = express();
+router(app);
+
+app.use(errorHandler);
 
 module.exports = app;
