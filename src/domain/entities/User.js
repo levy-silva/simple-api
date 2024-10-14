bc4f1de0e9f37356cfe725acacdf2c950fe2f84c
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 10:36:08 -0300

    fix: add missing Email Object Value into User entity

--- src/domain/entities/User.js
+++ src/domain/entities/User.js
@@ -1,4 +1,5 @@
 const Name = require("../object-values/Name.js");
+const Email = require("../object-values/Email.js");
 const Phone = require("../object-values/Phone.js");
 const Gender = require("../object-values/Gender.js");
 
@@ -6,7 +7,7 @@ class User {
     constructor({name, gender, email, phone,}, options = { required: true }) {
         this.name = new Name(name).execute(options);
         this.phone = new Phone(phone).execute(options);
-        this.email = new Gender(gender).execute(options);
+        this.email = new Email(email).execute(options);
         this.gender = new Gender(gender).execute(options);
     }
 }
