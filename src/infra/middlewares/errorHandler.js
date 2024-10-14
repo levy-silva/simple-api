d6e690caca750f89206a3057abd79038f33b8536
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 10:35:05 -0300

    refactor: enhance errorHandler to send a properly error message

--- src/infra/middlewares/errorHandler.js
+++ src/infra/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const ValidationHandler = require("../utils/helpers/ValidationHandler.js");
 
 function errorHandler(error, req, res, next) {
     if (error instanceof ValidationHandler) return error.report(res);
-    else return res.status(500).json({ message: "Erro interno do servidor" });
+    else return res.status(500).json({ message: `Erro interno do servidor: ${error.message}`});
 }
 
 module.exports = errorHandler;
