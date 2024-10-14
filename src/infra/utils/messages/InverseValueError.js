0efdaef914de4bfa625da4327aac72e4fb17631a
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 08:55:27 -0300

    feat: add InverseVAlueError to encapsulate invalid value error message

---
+++ src/infra/utils/messages/InverseValueError.js
@@ -0,0 +1,14 @@
+const ErrorMessager = require("../helpers/ErrorMessager.js");
+
+class InverseValueError extends ErrorMessager {
+    constructor(value, [...values]) {
+        const allowedValues = values.map((val, index) => {
+            if (index === (values.length - 1)) return val;
+            if (index === (values.length - 2)) return `${val} or `;
+            return `${val}, `;
+        }).join("");
+        super(`${value} is not valid. Only ${allowedValues} is allowed.`)
+    }
+}
+
+module.exports = InverseValueError;
