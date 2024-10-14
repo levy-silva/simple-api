11849fc36d8fad37dcb1436813c93366bc7c4e2a
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 10:32:20 -0300

    feat: add notValidEmail function to check if an email is not a valid email address

--- src/infra/utils/validators/notValidEmail.js
+++ src/infra/utils/validators/notValidEmail.js
@@ -1,5 +1,6 @@
 function notValidEmail(email) {
-    return  !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const validation = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    return !validation;
 }
 
 module.exports = notValidEmail;
