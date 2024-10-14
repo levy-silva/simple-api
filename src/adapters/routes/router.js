2eee1deba07b5b0e4965eff375dd06cbeadcd792
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 09:57:02 -0300

    feat: import userRoutes into main router entry point

--- src/adapters/routes/router.js
+++ src/adapters/routes/router.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const userRoutes = require("./userRoutes.js");
 
 const router = (app) => {
     app.route("/").get((req, res) => res.json({ message: "Root route listening." }));
-    app.use(express.json());
+    app.use(
+        express.json(),
+        userRoutes,
+    );
 }
 
 module.exports = router;
