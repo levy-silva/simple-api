9f4508028914dd3d05409646ac912896a9d8f8df
Levy Silva <levysilva.dev@gmail.com>
2024-10-14 09:02:03 -0300

    refactor: import Object Values and use it into User entity

--- src/domain/entities/User.js
+++ src/domain/entities/User.js
@@ -1,9 +1,13 @@
+const Name = require("../object-values/Name.js");
+const Phone = require("../object-values/Phone.js");
+const Gender = require("../object-values/Gender.js");
+
 class User {
-    constructor({name, gender, email, phone, unemployed}) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.gender = gender;
+    constructor({name, gender, email, phone,}, options = { required: true }) {
+        this.name = new Name(name).execute(options);
+        this.phone = new Phone(phone).execute(options);
+        this.email = new Gender(gender).execute(options);
+        this.gender = new Gender(gender).execute(options);
     }
 }
 
